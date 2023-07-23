af6b19fc2e2a389692f0abf69182d56e1edc2471
Sohaib <m.soh.khr@gmail.com>
2023-07-23 18:19:59 +0500

    responsive-main

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import BlogPage from './BlogPage';
+import MainBlog from './MainBlog';
+import 'https://kit.fontawesome.com/9c1c3f6a2e.js'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
+
 
 function App() {
   return (
     <div className="app">
-    
+      {/* <MainBlog /> */}
+      <BlogPage />
     </div>
   );
 }
