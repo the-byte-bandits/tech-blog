8776e167a5893949caf515638dc117a84fbfacc6
Android <shafiqshahzaib@gmail.com>
2023-07-24 14:36:53 +0500

    Blog Editor

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import BlogPage from './BlogPage';
-import MainBlog from './MainBlog';
 import 'https://kit.fontawesome.com/9c1c3f6a2e.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import blogEditor from './Blog-Editor/blogEditor';
 import './App.css';
-import Container from './adminComponents/Container/Container';
-import Menu from './adminComponents/SideMenu/Menu';
 
 
 
 function App() {
   return (
     <div className="app">
-
-      <Menu/>
-      <Container/>
-
-      {/* <MainBlog /> */}
-      <BlogPage />
+  <blogEditor/>
+     
 
     </div>
   );
