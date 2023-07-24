ea4a7fe9c9e491063dcb5b4523d157312064e97b
Android <shafiqshahzaib@gmail.com>
2023-07-24 15:19:27 +0500

    Jodit Editor Added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import 'https://kit.fontawesome.com/9c1c3f6a2e.js'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react'
 import blogEditor from './Blog-Editor/blogEditor';
-import './App.css';
-
-
-
 function App() {
+ 
   return (
-    <div className="app">
-  <blogEditor/>
-     
-
+    <div>
+      <blogEditor/>
     </div>
   );
 }
