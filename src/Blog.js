644ba922021fb3566c3bc14740886cd76ee724a9
Sohaib <m.soh.khr@gmail.com>
2023-07-24 19:33:39 +0500

    -

--- src/Blog.js
+++ src/Blog.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './Blog.css'
+import {Link} from "react-router-dom";
+
 
 export default function Blog() {
   return (
@@ -7,7 +9,9 @@ export default function Blog() {
         <div className="blog-container">
             <img src={require('./images/1.png')} alt="" />
             <div className="blog-content">
+              <Link to='/blog'>
                 <h1>Building your API stack</h1>
+              </Link>
                 <p>Lorem ipsum, dolor sit amet cons consectetur adipisicing elit. Quibusdam corrupti amet assumenda.</p>
                 <div className="blog-info">
                     <img src={require('./images/user-img.png')} alt=""  className='blog-user-img'/>
--- src/CommentsSection.js
+++ src/CommentsSection.js
@@ -6,11 +6,11 @@ function CommentsSection() {
   return (
     <div className="comments-section">
         <div className="comments-section-container">
-            <h1>Comments (6)</h1>
+            <h1>Reviews (6)</h1>
             <div className="add-comment">
                 <img src={require('./images/user-img.png')} alt="" />
                 <form action="">
-                    <input type="text" placeholder="Add a comment..." />
+                    <input type="text" placeholder="Add a review..." />
                     <button><i class="fa-solid fa-paper-plane"></i></button>
                 </form>
                 
