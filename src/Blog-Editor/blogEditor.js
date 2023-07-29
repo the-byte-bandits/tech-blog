e07cb4c54c355a9b763b7249631180f04c87c5d5
Android <shafiqshahzaib@gmail.com>
2023-07-29 12:10:39 +0500

    Updated Editor CSS

--- src/Blog-Editor/blogEditor.js
+++ src/Blog-Editor/blogEditor.js
@@ -2,6 +2,7 @@ import React, { useState, useRef } from "react";
 import JoditEditor from "jodit-react";
 import HTMLReactParser from "html-react-parser";
 import AddCoverPhoto from '../addCoverPhoto'
+import './blogEditor.css'
 export default function BlogEditor() {
   const editor = useRef(null);
   const [content, setContent] = useState("");
@@ -9,7 +10,7 @@ export default function BlogEditor() {
   return (
     <div>
 
-        <h1>Blog Editor</h1>
+        <h1 className="editorheading">Blog Editor</h1>
         <AddCoverPhoto/>
       <JoditEditor
         ref={editor}
