c77ec8ae61a5d87ccca92c56964248050b58ac58
Android <shafiqshahzaib@gmail.com>
2023-07-29 12:05:53 +0500

    Added Cover Photo Section

--- src/Blog-Editor/blogEditor.js
+++ src/Blog-Editor/blogEditor.js
@@ -1,13 +1,16 @@
 import React, { useState, useRef } from "react";
 import JoditEditor from "jodit-react";
 import HTMLReactParser from "html-react-parser";
-
+import AddCoverPhoto from '../addCoverPhoto'
 export default function BlogEditor() {
   const editor = useRef(null);
   const [content, setContent] = useState("");
 
   return (
     <div>
+
+        <h1>Blog Editor</h1>
+        <AddCoverPhoto/>
       <JoditEditor
         ref={editor}
         value={content}
---
+++ src/addCoverPhoto.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import PhotoCamera from '@material-ui/icons/PhotoCamera';
+import IconButton from '@material-ui/core/IconButton';
+import './Blog-Editor/coverphoto.css'
+const App = () => {
+
+return (
+	<div  className='maincoverphoto' 
+	
+    >
+	<div className='coverheading'>
+		<h3>Add a Cover Photo for Blog</h3> <br />
+	</div>
+	<input
+		type="file"
+		accept="image/*"
+		style={{ display: 'none' }}
+		id="contained-button-file"
+	/>
+	<label htmlFor="contained-button-file">
+		<Button variant="contained" color="primary" component="span">
+		Upload
+		</Button>
+	</label>
+	<h3>  OR  </h3>
+	<input accept="image/*" id="icon-button-file"
+		type="file" style={{ display: 'none' }} />
+	<label htmlFor="icon-button-file">
+		<IconButton color="primary" aria-label="upload picture"
+		component="span">
+		<PhotoCamera />
+		</IconButton>
+	</label>
+	</div>
+);
+}
+
+export default App;
