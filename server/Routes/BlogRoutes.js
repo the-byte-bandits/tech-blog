810a3ff37ff52f9c49221fa57eb5f5d54b52a97a
Android <shafiqshahzaib@gmail.com>
2023-08-10 16:47:33 +0500

    Internal Server Error

--- server/Routes/BlogRoutes.js
+++ server/Routes/BlogRoutes.js
@@ -2,21 +2,33 @@ const express = require("express");
 const blogModel = require("../Models/Blog");
 const mongoose = require('mongoose');
 const router = express.Router();
-const bodyParser=require("body-parser")
-
-app.use(bodyParser.urlencoded({extended:true}))
 
 router.get('/get-all-blogs', async (req, res) => {
-
-
-    
-})
+  // Implement logic to fetch and return all blogs
+  try {
+    const blogs = await blogModel.find(); // Assuming you have a model named 'blogModel'
+    res.json(blogs);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
 
 router.post('/write-blog', async (req, res) => {
-    
-    
+  const { category, title, content } = req.body;
 
+  if (!title || !content || !category) {
+    return res.status(422).json({ error: "Please provide Blog Title, Content, and Category" });
+  }
 
+  try {
+    const newBlog = new blogModel({ category, title, content });
+    const savedBlog = await newBlog.save();
+    res.json(savedBlog);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
 });
 
 module.exports = router;
--- src/BlogEditor.js
+++ src/BlogEditor.js
@@ -1,9 +1,6 @@
-import './BlogEditor.css'
-import React, { useState, useRef } from "react";
-import JoditEditor from "jodit-react";
-import HTMLReactParser from "html-react-parser";
-import Dropdown from 'react-dropdown';
-import 'react-dropdown/style.css';
+import './BlogEditor.css';
+import React, { useState, useRef } from 'react';
+import JoditEditor from 'jodit-react';
 import Swal from 'sweetalert2';
 import {
   CDropdown,
@@ -13,23 +10,10 @@ import {
 } from '@coreui/react';
 import ImageUpload from './ImageUpload';
 
-
-
-
-
-
-
 export default function BlogEditor() {
   const editor = useRef(null);
-  const [content, setContent] = useState("");
-
-  
-
-
-  const _onSelect = (option) => {
-    console.log("Selected option:", option);
-    // Do something with the selected option if needed
-  };
+  const [content, setContent] = useState('');
+  const [blog, setBlog] = useState({ category: '', title: '', content: '' });
 
   const showInputAlert = () => {
     Swal.fire({
@@ -40,53 +24,79 @@ export default function BlogEditor() {
       confirmButtonText: 'Submit',
       showLoaderOnConfirm: true,
       preConfirm: (category) => {
-        // Handle the category value here (e.g., save to state, display, etc.)
-        // For this example, we simply display an alert with the entered value.
-        
+        setBlog({ ...blog, category }); // Set the selected category
       },
-      allowOutsideClick: () => !Swal.isLoading()
+      allowOutsideClick: () => !Swal.isLoading(),
     });
   };
 
+  // Data submission / processing 
 
+  const handleInputs = (e) => {
+    const { name, value } = e.target;
+    setBlog({ ...blog, [name]: value });
+  };
+
+  const PostData = async (e) => {
+    e.preventDefault();
+    const { category, title, content } = blog;
+    
+  };
 
   return (
-    <div className='correction blog-editor'>
-
-    
-
+    <div className="correction blog-editor">
       <form>
-            <div >
-                <div className="blog-dropdown">
-                    <CDropdown>
-                        <CDropdownToggle color="secondary">Select Blog Category</CDropdownToggle>
-                        <CDropdownMenu>
-                            <CDropdownItem href="#">Latest Tech</CDropdownItem>
-                            <CDropdownItem href="#">AI</CDropdownItem>
-                            <CDropdownItem href="#">Programming</CDropdownItem>
-                            <CDropdownItem href="#">Mobiles</CDropdownItem>
-                            <CDropdownItem href="#">Laptops</CDropdownItem>
-                            <CDropdownItem onClick={showInputAlert}>Others</CDropdownItem>
-                        </CDropdownMenu>
-                    </CDropdown>
-                </div>
-                <br />
-                <form>
-                    <div className="form-group">
-                        <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter blog title" />
-                    </div>
-                </form>
-                <br />
-                <ImageUpload />
-                <br />
-                <JoditEditor
-                    ref={editor}
-                    value={content}
-                    onChange={(newContent) => setContent(newContent)}
-                />
-
-                {/* <div>{HTMLReactParser(content)}</div> */}
+        <div>
+          <div className="blog-dropdown">
+            <CDropdown>
+              <CDropdownToggle color="secondary">
+                Select Blog Category
+              </CDropdownToggle>
+              <CDropdownMenu>
+                <CDropdownItem href="#">Latest Tech</CDropdownItem>
+                <CDropdownItem href="#">AI</CDropdownItem>
+                <CDropdownItem href="#">Programming</CDropdownItem>
+                <CDropdownItem href="#">Mobiles</CDropdownItem>
+                <CDropdownItem href="#">Laptops</CDropdownItem>
+                <CDropdownItem onClick={showInputAlert}>Others</CDropdownItem>
+              </CDropdownMenu>
+            </CDropdown>
+          </div>
+          <br />
+          <form method="POST">
+            <div className="form-group">
+              <input
+                name="title"
+                type="text"
+                className="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                placeholder="Enter blog title"
+                value={blog.title}
+                onChange={handleInputs}
+              />
             </div>
-        </form>
+          </form>
+          <br />
+          <ImageUpload />
+          <br />
+          <JoditEditor
+            ref={editor}
+            value={content}
+            onChange={(newContent) => setContent(newContent)}
+            name="content"
+            value={blog.content}
+          />
+          <div>
+            <input
+              type="submit"
+              name="submit"
+              value="register"
+              onClick={PostData}
+            />
+          </div>
+        </div>
+      </form>
     </div>
-)}
+  );
+}
