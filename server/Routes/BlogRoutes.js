ff68cb462b665115a6ea5e8df83038863e4206b8
Android <shafiqshahzaib@gmail.com>
2023-08-10 11:34:15 +0500

    Initlized

--- server/Routes/BlogRoutes.js
+++ server/Routes/BlogRoutes.js
@@ -2,13 +2,21 @@ const express = require("express");
 const blogModel = require("../Models/Blog");
 const mongoose = require('mongoose');
 const router = express.Router();
+const bodyParser=require("body-parser")
+
+app.use(bodyParser.urlencoded({extended:true}))
 
 router.get('/get-all-blogs', async (req, res) => {
 
+
+    
 })
 
 router.post('/write-blog', async (req, res) => {
     
+    
+
+
 });
 
 module.exports = router;
